c04b87b61edc873abef53d4e0c661300c63564d7
Upeka Sewwandi <upekasewwandi81@gmail.com>
2026-03-06 07:36:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const selectBox = document.getElementById('select-box');
-const imageContainer = document.querySelector('.image-container');
+const selectBox = document.getElementById('select-box')
+const imageContainer = document.querySelector('.image-container')
+const characters = ['Garfield', 'Odie', 'Nermal', 'Pooky', 'Jon', 'Scooby', 'Shaggy', 'Velma']
 
-const characters = ['garfield', 'jon', 'nermal', 'odie', 'pooky','scooby','shaggy','velma'];
+for (let character of characters) {
+    const optionEl = document.createElement("option")
+    optionEl.value = character.toLowerCase()
+    optionEl.textContent = character
 
-characters.forEach((character) => {
-    const optionEl = document.createElement('option');
-    optionEl.value = character.toLowerCase();
-    optionEl.textContent = character;
+    selectBox.appendChild(optionEl)
+}
 
-    selectBox.appendChild(optionEl);
-});
+const initialImage = document.createElement('img')
+
+initialImage.src = `/assets/images/${selectBox.childNodes[0].value}.png`
+initialImage.width = '300'
+imageContainer.append(initialImage)
 
 selectBox.addEventListener('change', (e) => {
-    (imageContainer.childElementCount > 0) &&  imageContainer.childNodes[0].remove()
-        
-    const imageEl = document.createElement('img');
-    imageEl.src = `assets/${e.target.value}.jpg`;
-    imageEl.width="300";
+    (imageContainer.childElementCount > 0) && imageContainer.childNodes[0].remove()
 
-    imageContainer.appendChild(imageEl);
-});
+    const imageEl = document.createElement('img')
+    imageEl.src = `/assets/images/${e.target.value}.png`
+    imageEl.width = '300'
+
+    imageContainer.appendChild(imageEl)
+})
